82f79768aa52f23fe2815ae3a96b5c4d011d2162
Kevin Johnson Gulane <126481785+KJGulane@users.noreply.github.com>
2023-04-03 18:29:12 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,98 @@
+console.log("Hello World!");
+
+let name  = "Kevin Johnson";
+let lastName = "Gulane";
+let age = 30;
+console.log (`First name: ${name}`);
+console.log (`Last name: ${lastName}`);
+console.log (`Last name: ${age}`);
+
+
+NO.2
+let x= 40;
+let y= 60;
+console.log (x == y)//false
+console.log (x += y)//100
+console.log (x -= y)//40
+console.log (x *= y)//2400
+console.log (x /= y)//40
+console.log (x <= y)//true
+console.log (x >= y)//false
+console.log (x !== y)//true
+
+
+NO.3
+let colors = prompt ("Choose Color Between Black, Violet and Pink" );
+    switch (colors)
+    {
+        case (colors = "Black"):
+        console.log ("Black Accepted");
+        break;
+
+        case (colors = "Violet"):
+        console.log ("Violet Accepted");
+        break;
+
+        case (colors = "Pink"):
+        console.log ("Pink Accepted");
+        break;
+
+        case (colors = "black"):
+        console.log ("black Accepted");
+        break;
+
+        case (colors = "violet"):
+        console.log ("violet Accepted");
+        break;
+
+        case (colors = "pink"):
+        console.log ("pink Accepted");
+        break;
+
+        default:
+            console.log ("Invalid Entry");
+        
+     }
+
+
+NO.4
+let userInput = prompt ("Give me a number");
+
+
+function oddOrEven (num) {
+    if (isNaN (num) || typeof parseInt(num) !== "number")
+    
+    {
+        return `Unexpected Input`;
+    }
+    else if (num == ''){
+        return "Please input inside the box"
+    }
+
+    else {
+        if (num % 2 == 0)
+        {
+            return ("even number");
+        }
+        else {
+            return ("odd number");
+        }
+    }
+}
+
+console.log(oddOrEven(userInput));
+
+
+NO.5
+
+for (let i = 20; i >= 20; i++ )
+{
+    if (i % 2 == 0)
+    {
+    console.log (`Even number ${i}`);
+    }
+    else {
+        console.log (`Odd number ${i}`)
+    }
+}
+
