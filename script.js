ad1a3fd0e0a12672a70df63150884ad35718be05
Kevin Johnson Gulane <126481785+KJGulane@users.noreply.github.com>
2023-04-05 22:00:41 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,91 +1,100 @@
-console.log("Hello World!");
+// No. 1
+// console.log("Hello World!");
 
-let name  = "Kevin Johnson";
-let lastName = "Gulane";
-let age = 30;
-console.log (`First name: ${name}`);
-console.log (`Last name: ${lastName}`);
-console.log (`Last name: ${age}`);
+// let name  = "Kevin Johnson";
+// let lastName = "Gulane";
+// let age = 30;
+// console.log (`First Name: ${name}`);
+// console.log (`Last Name: ${lastName}`);
+// console.log (`Age: ${age}`);
+
+// let hobby = ["Biking", "Swimming", "Dancing"];
+// let city = "Lincoln House Number 23";
+// let state = "Nebraska";
+// let Street = "Washington";
+// console.log("Hobbies:",hobby);
+// console.log("City:",city);
+// console.log("State:",state);
+// console.log("Street:",Street);
+
+// //NO.2
+// let x= 40;
+// let y= 60;
+// console.log (x == y)//false
+// console.log (x += y)//100
+// console.log (x -= y)//40
+// console.log (x *= y)//2400
+// console.log (x /= y)//40
+// console.log (x <= y)//true
+// console.log (x >= y)//false
+// console.log (x !== y)//true
 
 
-NO.2
-let x= 40;
-let y= 60;
-console.log (x == y)//false
-console.log (x += y)//100
-console.log (x -= y)//40
-console.log (x *= y)//2400
-console.log (x /= y)//40
-console.log (x <= y)//true
-console.log (x >= y)//false
-console.log (x !== y)//true
+// //NO.3
+// let colors = prompt ("Choose Color Between Black, Violet and Pink" );
+//     switch (colors)
+//     {
+//         case (colors = "Black"):
+//         console.log ("Black Accepted");
+//         break;
 
+//         case (colors = "Violet"):
+//         console.log ("Violet Accepted");
+//         break;
 
-NO.3
-let colors = prompt ("Choose Color Between Black, Violet and Pink" );
-    switch (colors)
-    {
-        case (colors = "Black"):
-        console.log ("Black Accepted");
-        break;
+//         case (colors = "Pink"):
+//         console.log ("Pink Accepted");
+//         break;
 
-        case (colors = "Violet"):
-        console.log ("Violet Accepted");
-        break;
+//         case (colors = "black"):
+//         console.log ("black Accepted");
+//         break;
 
-        case (colors = "Pink"):
-        console.log ("Pink Accepted");
-        break;
+//         case (colors = "violet"):
+//         console.log ("violet Accepted");
+//         break;
 
-        case (colors = "black"):
-        console.log ("black Accepted");
-        break;
+//         case (colors = "pink"):
+//         console.log ("pink Accepted");
+//         break;
 
-        case (colors = "violet"):
-        console.log ("violet Accepted");
-        break;
-
-        case (colors = "pink"):
-        console.log ("pink Accepted");
-        break;
-
-        default:
-            console.log ("Invalid Entry");
+//         default:
+//             console.log ("Invalid Entry");
         
-     }
+//      }
 
 
-NO.4
-let userInput = prompt ("Give me a number");
+// //NO.4
+// let userInput = prompt ("Give me a number");
 
 
-function oddOrEven (num) {
-    if (isNaN (num) || typeof parseInt(num) !== "number")
+// function oddOrEven (num) {
+//     if (isNaN (num) || typeof parseInt(num) !== "number")
     
-    {
-        return `Unexpected Input`;
-    }
-    else if (num == ''){
-        return "Please input inside the box"
-    }
+//     {
+//         return `Unexpected Input`;
+//     }
+//     else if (num == ''){
+//         return "Please input inside the box"
+//     }
 
-    else {
-        if (num % 2 == 0)
-        {
-            return ("even number");
-        }
-        else {
-            return ("odd number");
-        }
-    }
-}
+//     else {
+//         if (num % 2 == 0)
+//         {
+//             return ("even number");
+//         }
+//         else {
+//             return ("odd number");
+//         }
+//     }
+// }
 
-console.log(oddOrEven(userInput));
+// console.log(oddOrEven(userInput));
 
 
-NO.5
+//NO.5
 
-for (let i = 20; i >= 20; i++ )
+for (let i = 0; i <= 20; i++ )
 {
     if (i % 2 == 0)
     {
